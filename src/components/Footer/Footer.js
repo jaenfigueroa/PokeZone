8f0d7031f5b0f8394ca17eb4c5029f341aac5c30
jaen <jaenfigueroa03042000@gmail.com>
2023-01-03 20:08:53 -0500

    terminado

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ export const Footer = () => {
   return (
     <footer className='footer'>
 
-      <img className='imagen-footer' src='https://lh3.googleusercontent.com/9FHOk79iiGEisBJxkU9smRi8CUKagEkt_yl7T7z9mEBHypSg5sblsGkv1YOxj-4vCpVbYUeo7dC6q2rxiHn9fNlcBxXGabLd7RpsNC6MHrwCRw=rw-e365-w1440'></img>
+      <img className='imagen-footer' src='https://lh3.googleusercontent.com/9FHOk79iiGEisBJxkU9smRi8CUKagEkt_yl7T7z9mEBHypSg5sblsGkv1YOxj-4vCpVbYUeo7dC6q2rxiHn9fNlcBxXGabLd7RpsNC6MHrwCRw=rw-e365-w1440' alt='imagen de pikachu y sus amigos'></img>
       <p>Developed by <a href='https://www.jaenfigueroa.com' target='_blank' rel="noreferrer">Jaen Figueroa</a> &copy; 2023</p>
 
       <div className='footer__redes-contenedor'>
