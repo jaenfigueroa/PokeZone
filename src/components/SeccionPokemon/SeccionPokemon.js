8efe9cd8a6e17eca7e6d143d8b858ee52fd2a8af
jaen <jaenfigueroa03042000@gmail.com>
2023-01-05 10:36:11 -0500

    terminado

--- src/components/SeccionPokemon/SeccionPokemon.js
+++ src/components/SeccionPokemon/SeccionPokemon.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useState} from 'react'
 import { Pokemon } from '../Pokemon/Pokemon'
 import './SeccionPokemon.css'
 import {BarLoader} from "react-spinners";
