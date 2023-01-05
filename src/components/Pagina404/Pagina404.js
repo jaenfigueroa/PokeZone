2dd846c05af110547e7fb8be9ec4ffc6c948f6e8
jaen <jaenfigueroa03042000@gmail.com>
2023-01-04 19:45:25 -0500

    proyecto terminado

--- src/components/Pagina404/Pagina404.js
+++ src/components/Pagina404/Pagina404.js
@@ -1,20 +1,12 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import './Pagina404.css'
 import { Link } from 'react-router-dom'
 
 export const Pagina404 = () => {
 
-  useEffect(()=>{
-    document.getElementById('miVideo').play()
-  })
-
-
   return (
     <div className='caja__error404'>
 
-      <video  className='video' autoplay loop muted id='miVideo'>
-        <source src="https://storage.googleapis.com/pgoblog/seasons-mythical-wishes/Hero%20Trailer/PGO_S9_Launch_16x9_WebHeader_v1.mp4" type="video/mp4"/>
-      </video>
 
       <div className='caja__contenedor'>
         <h4 className='error404__titulo'>¡Ups! Página no encontrada</h4>
