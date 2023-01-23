6dbe4dd1b0cf9ac574e1761c1833eaa9117490c2
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2023-01-23 18:18:48 -0500

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,7 +14,7 @@ export const Footer = ({idioma, setIdioma}) => {
     <footer className='footer'>
 
       <img className='imagen-footer' src='https://lh3.googleusercontent.com/9FHOk79iiGEisBJxkU9smRi8CUKagEkt_yl7T7z9mEBHypSg5sblsGkv1YOxj-4vCpVbYUeo7dC6q2rxiHn9fNlcBxXGabLd7RpsNC6MHrwCRw=rw-e365-w1440' alt='imagen de pikachu y sus amigos'></img>
-      <p>{t('derechos')} <a href='https://www.jaenfigueroa.com' target='_blank' rel="noreferrer">Jaen Figueroa</a> &copy; 2023</p>
+      <p>{t('derechos')} <a href='https://github.com/jaenfigueroa' target='_blank' rel="noreferrer">Jaen Figueroa</a> &copy; 2023</p>
 
       <div className='footer__redes-contenedor'>
         <a href='https://www.facebook.com/JaenDeveloper/' target='_blank' rel="noreferrer"><i className="fa-brands fa-facebook"></i></a>
